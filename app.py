c5066608a389c9406f5d2f0ce3130a0e8051d12b
coinerwh <coinerwh@gmail.com>
2020-10-29 22:38:12 -0700

    Added signup route handler and signup template

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def root():
     return render_template('index.j2')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.j2')
+
 if __name__ == '__main__':
     app.run(port=5000)
