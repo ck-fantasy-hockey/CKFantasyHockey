78912ef3ad89830cc4d86a813127c278aa7dd860
George Kochera <george.kochera@gmail.com>
2020-10-30 17:31:20 -0400

    changed from CDN to npm package react react-dom

---
+++ src/join_team.js
@@ -0,0 +1,5 @@
+export default class JoinTeam extends React.Component {
+    render() {
+        return <h1>JOIN TEAM</h1> 
+    }
+}
--- src/page.js
+++ src/page.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import TeamView from './team_view.js'
 import DATA from '../json/data.js'
 
