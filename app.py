acd8d0015e716ec76882b9039b2b7edc60eb326f
coinerwh <coinerwh@gmail.com>
2020-10-30 22:32:07 -0700

    fixing merge issue

--- app.py
+++ app.py
@@ -6,19 +6,30 @@ app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+# Data Transportation
+
+dataFromServer = {}
+
 # Routes
 
 @app.route('/')
 def root():
-    return render_template('index.j2', page="landing_page")
+    return render_template('index.j2', page="landing_page", dataFromServer=dataFromServer)
 
 @app.route('/team-view')
 def team_view():
-    return render_template('index.j2', page="team_view")
+    return render_template('index.j2', page="team_view", dataFromServer=dataFromServer)
 
 @app.route('/join-league')
 def join_team():
-    return render_template('index.j2', page="join_league")
+    return render_template('index.j2', page="join_league", dataFromServer=dataFromServer)
+
+@app.route('/create-team')
+def create_team():
+    dataFromServer = {
+        "leagueID": request.args.get('leagueID') 
+        }
+    return render_template('index.j2', page="create_team", dataFromServer=dataFromServer)
 
 @app.route('/signup')
 def signup():
