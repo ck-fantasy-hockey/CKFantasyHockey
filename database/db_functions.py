1fab8c7315686c9a7c1303204f572b175fd3ec93
George Kochera <george.kochera@gmail.com>
2020-11-10 20:15:19 -0500

    house all database functions in this python file

--- database/db_functions.py
+++ database/db_functions.py
@@ -5,24 +5,15 @@ import os
 def establish_connection():
 
     # Get the environment variables where sensitive logon information securely stored
-    if os.environ.get('FLASK_ENV') == 'DEVELOPMENT':
-        config = {
+    config = {
         'user': os.environ.get('DB_USER'),
         'password': os.environ.get('DB_PASSWORD'),
         'host': os.environ.get('DB_HOST'),
         'database': os.environ.get('DB_DATABASE')
         }
-    else:
+    if os.environ.get('FLASK_ENV') == 'production':
+        config['ssl_disabled'] = True
 
-        config = {
-        'user': 'bcfa980b82b70c',
-        'password': '7d8b0cdd',
-        'host': 'us-cdbr-east-02.cleardb.com',
-        'database': 'heroku_356a05812d22760',
-        'ssl_disabled': True
-        }
-
-    # mysql://bcfa980b82b70c:7d8b0cdd@us-cdbr-east-02.cleardb.com/heroku_356a05812d22760?reconnect=true
     # Make the connection
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
