30c01b8598cf7a080fcf746a4c3636036650efcf
George Kochera <george.kochera@gmail.com>
2020-11-24 00:03:50 -0500

    GoalieRow and PlayerRow now fully express data received from the TeamView state (which is popluated from the DB)

--- src/team/goalie_row.js
+++ src/team/goalie_row.js
@@ -3,24 +3,24 @@ import PlayerFunctions from "./player_functions.js";
 export default class GoalieRow extends React.Component {
     render() {
         return <tr>
-            <td>{this.props.name}</td>
-            <td>-</td>
-            <td>-</td>
+            <td>{this.props.playerName}</td>
+            <td>{this.props.team}</td>
+            <td>{this.props.status}</td>
             <td>{this.props.playerID}</td>
             <td>{this.props.position}</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
+            <td>{this.props.gamesPlayed}</td>
+            <td>{this.props.goals}</td>
+            <td>{this.props.assists}</td>
+            <td>{this.props.points}</td>
+            <td>{this.props.wins}</td>
+            <td>{this.props.losses}</td>
+            <td>{this.props.overtimeLosses}</td>
+            <td>{this.props.shutOuts}</td>
+            <td>{this.props.goalsAllowedAverage}</td>
+            <td>{this.props.goalsAllowed}</td>
+            <td>{this.props.saves}</td>
+            <td>{this.props.savePercentage}</td>
+            <td>{this.props.minutesPlayed}</td>
             <td>                
                 <PlayerFunctions />
             </td>
--- src/team/player_row.js
+++ src/team/player_row.js
@@ -3,22 +3,23 @@ import PlayerFunctions from './player_functions.js'
 export default class PlayerRow extends React.Component {
     render() {
         return <tr>
-            <td>{this.props.name}</td>
-            <td>-</td>
-            <td>-</td>
+            <td>{this.props.playerName}</td>
+            <td>{this.props.team}</td>
+            <td>{this.props.status}</td>
             <td>{this.props.playerID}</td>
             <td>{this.props.position}</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
+            <td>{this.props.gamesPlayed}</td>
+            <td>{this.props.goals}</td>
+            <td>{this.props.assists}</td>
+            <td>{this.props.points}</td>
+            <td>{this.props.shootoutGoals}</td>
+            <td>{this.props.hatTricks}</td>
+            <td>{this.props.plusMinus}</td>
+            <td>{this.props.pointsPerGame}</td>
+            <td>{this.props.shorthandedGoals}</td>
+            <td>{this.props.penaltyMinutes}</td>
+            <td>{this.props.blocks}</td>
+
             <td>
                 <PlayerFunctions />
             </td>
