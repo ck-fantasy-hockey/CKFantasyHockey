ea7694a2fd6d52f739bc68102b51ba1f572d013d
George Kochera <kocherag@oregonstate.edu>
2020-11-29 20:04:53 -0500

    /drop-player route works

--- app.py
+++ app.py
@@ -195,8 +195,8 @@ def drop_player():
 
     # Get the information from client
     sent_info = request.get_json()
-    print(sent_info)
-    return jsonify({'response': True})
+    
+    return jsonify({'response': database.db_functions.drop_player_from_team(sent_info)})
 
 if __name__ == '__main__':
     # Will set port to 5000 on local machine, but allow Heroku to bind on deployment.
