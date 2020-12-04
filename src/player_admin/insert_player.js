ef4cd2d9d18c50fd5b0b704ee1fc4941d221d8af
coinerwh <coinerwh@gmail.com>
2020-12-03 18:40:50 -0800

    input fields added

--- src/player_admin/insert_player.js
+++ src/player_admin/insert_player.js
@@ -1,19 +1,19 @@
 export default class InsertPlayer extends React.Component {
     render() {
         return <tr>
-            {/* <td>{this.props.playerID}</td>
-            <td>{this.props.playerName}</td>
-            <td>{this.props.team}</td>
-            <td>{this.props.status}</td>
-            <td>{this.props.position}</td>
-            <td>{this.props.goals}</td>
-            <td>{this.props.assists}</td>
-            <td>{this.props.points}</td>
-            <td>{this.props.gameWinningGoals}</td>
-            <td>{this.props.penaltyMinutes}</td>
-            <td>{this.props.minutesPlayed}</td>
-            <td>{this.props.blocks}</td>
-            <td><button className="player_button">Update</button><button className="player_button">Delete</button></td> */}
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><input type="text" id="fname" name="fname" /></td>
+            <td><button className="player_button">Save</button></td>
         </tr>
     }
 }
