7c3ecf656b18c68d5518027611278fefab5bfacd
George Kochera <kocherag@oregonstate.edu>
2020-12-03 19:44:35 -0500

    styled delete button and removed reset password... for now

--- src/user_management/user_row.js
+++ src/user_management/user_row.js
@@ -11,8 +11,7 @@ export default class UserRow extends React.Component {
             <td className='hide-on-med-and-down'>{this.props.sessionID}</td>
             <td className='hide-on-med-and-down'>{this.props.sessionExpires}</td>
             <td>
-                <button onClick={() => {this.props.deleteUser(this.props.userID)}}>Delete</button>
-                <button>Reset Password</button>
+                <button className='btn' onClick={() => {this.props.deleteUser(this.props.userID)}}>Delete</button>
             </td>
         </tr>
     }
