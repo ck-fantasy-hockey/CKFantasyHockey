584546271df46973ba85ca4cc15e7561c3f3ba49
coinerwh <coinerwh@gmail.com>
2020-12-03 17:02:31 -0800

    initial UI

---
+++ src/player_admin.js
@@ -0,0 +1,28 @@
+import Players from './league/teams.js'
+import PlayerInformation from './league/league_information.js'
+import NavBar from './nav_bar.js'
+
+// window.history.replaceState({}, document.title, "/league-view");
+
+class PlayerView extends React.Component {
+    constructor() {
+        super();
+        this.state = dataFromServer;
+        console.log(this.state)
+    }
+
+    render() {
+        return <div className='background-filter'>
+            <NavBar />
+            <div className='container'>
+                <PlayerInformation data={this.state.leaguedata} />
+                <Players data={this.state.teamdata} />
+            </div>
+        </div>
+    }
+}
+
+ReactDOM.render(
+    <PlayerView />,
+    document.getElementById('root')
+  );
